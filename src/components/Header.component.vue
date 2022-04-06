8b8ca95e09eb121ca866ec09b99879cc37d57ddc
jaime <jaime264@gmail.com>
2022-04-06 03:22:03 -0500

    home finish

--- src/components/Header.component.vue
+++ src/components/Header.component.vue
@@ -1,35 +1,41 @@
 <template>
-    <header>
-        <div class="container-logo">
-            <img width="215" height="50" src="@/assets/images/logo.png" alt="logo"/>
-        </div>
-        <div class="menu-navigation">
-            <nav>
-                <ul>
-                    <li>
-                        <router-link to="/">INICIO</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/nosotros">NOSOTROS</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/contactanos">CONTÁCTANOS</router-link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div class="menu-bar">
-            <div class="count-likes">
-                <img width="48" height="48" src="@/assets/images/heart.svg">
-                <span class="counter-likes">0</span>
-            </div>
-            <div class="container-items">
-                <img class="hamburguer-button" width="32" src="@/assets/images/hamburguer.svg" @click="openMenu()">
-                <div class="items-menu">
-                </div>
-            </div>
-        </div>
-    </header>
+  <header class="row">
+    <div class="container-logo">
+      <img width="215" height="50" src="@/assets/images/logo.png" alt="logo" />
+    </div>
+
+    <div class="menu-navigation">
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">INICIO</router-link>
+          </li>
+          <li>
+            <router-link to="/nosotros">NOSOTROS</router-link>
+          </li>
+          <li>
+            <router-link to="/contactanos">CONTÁCTANOS</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="menu-bar">
+      <div class="count-likes">
+        <img width="48" height="48" src="@/assets/images/heart.svg" />
+        <span class="counter-likes">0</span>
+      </div>
+      <div class="container-items">
+        <img
+          class="hamburguer-button"
+          width="32"
+          src="@/assets/images/hamburguer.svg"
+          @click="openMenu()"
+        />
+        <div class="items-menu"></div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -37,85 +43,96 @@ export default {
   setup() {
     return {};
   },
-  methods:{
-    openMenu(){
-        console.log("abrir menú")
-    }
-  }
+  methods: {
+    openMenu() {
+      console.log("abrir menú");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-header{
-    width: 100%;
-    background: #0d0d0d;
-    display: flex !important;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0px 130px;
-    height: 130px;
-    .menu-navigation{
-        nav{
-            ul{
-                display: flex;
-                list-style: none;
-                margin: 0;
-                padding: 0;
-                li{
-                    padding: 28px 35px;
-                    color: #ffffff;
-                    font-size: 16px;
-                    position: relative;
-                    a{
-                        color: inherit !important;
-                        text-decoration: none;
-                        &.router-link-exact-active{
-                            color: #e0006c !important;
-                        }
-                        &:hover{
-                            color: #e0006c !important;
-                        }
-                    }
-                    &:before {
-                        content: "\2022";
-                        position: absolute;
-                        top: 48%;
-                        left: 0;
-                        margin-top: -13px;
-                        color: #e0006c;
-                        display: inline-block;
-                        font-size: 20px;
-                    }
-                }
-            }
-        }
-    }
-    .menu-bar{
+header {
+  background: #0d0d0d;
+  display: flex !important;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 130px;
+  height: 130px;
+  .menu-navigation {
+    nav {
+      ul {
         display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 16px;
-        .count-likes{
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            gap: 8px;
-            .counter-likes{
-                color: #ffffff;
-                font-size: 18px;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        li {
+          padding: 28px 35px;
+          color: #ffffff;
+          font-size: 16px;
+          position: relative;
+          a {
+            color: inherit !important;
+            text-decoration: none;
+            &.router-link-exact-active {
+              color: #e0006c !important;
             }
-        }
-        .container-items{
-            .hamburguer-button{
-                cursor: pointer;
+            &:hover {
+              color: #e0006c !important;
             }
+          }
+          &:before {
+            content: "\2022";
+            position: absolute;
+            top: 48%;
+            left: 0;
+            margin-top: -13px;
+            color: #e0006c;
+            display: inline-block;
+            font-size: 20px;
+          }
         }
-        .navbar-toggler{
-            border-color: transparent;
-            color: #ffffff
-        }
+      }
     }
+  }
+  .menu-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 16px;
+    .count-likes {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 8px;
+      .counter-likes {
+        color: #ffffff;
+        font-size: 18px;
+      }
+    }
+    .container-items {
+      .hamburguer-button {
+        cursor: pointer;
+      }
+    }
+    .navbar-toggler {
+      border-color: transparent;
+      color: #ffffff;
+    }
+  }
 }
 
+@media (max-width: 1060px) {
+  header {
+    padding: 0px 100px;
+  }
+  .menu-navigation {
+    display: none;
+  }
+  .menu-bar{
+      padding-left: 3rem;
+  }
+  
+}
 </style>
--- src/components/card.component.vue
+++ src/components/card.component.vue
@@ -1,7 +1,16 @@
 <template >
   <div class="card-model" @click="goTo(model.id_ads)">
-    <img width="290" :src="'https://api.luxarymodels.com/public/images/models/'+model.photo1">
-    <span>{{model.username}}</span>
+    <img class="photo" width="290" :src="'https://api.luxarymodels.com/public/images/models/'+model.photo1">
+    <span class="pais"><img
+        class="pais-img"
+        :src="require(`@/assets/images/` + model.flag + `.svg`)"
+        alt=""
+      /> {{model.username}}, {{model.age}} años.</span>
+    <span class="location"><img
+        class="location-img"
+        src="@/assets/images/location.svg"
+        alt=""
+      />{{model.district}}, {{model.address}}</span>
   </div>
 </template>
 
@@ -23,11 +32,34 @@ export default {
 </script>
 
 <style lang="scss">
-.card-model{
+.card-model {
   display: flex;
   flex-direction: column;
-  span{
-      color: white;
+  
+  .photo{
+      margin-bottom: 5px;
+  }
+
+  .pais {
+    font-size: 14px;
+    &-img {
+      height: 22.7px;
+      width: 22.7px;
+      margin-right: 5px;
+    }
+  }
+
+  .location {
+    font-size: 12px;
+    &-img {
+      height: 15px;
+      width: 15px;
+      margin-right: 5px;
+    }
+  }
+
+  span {
+    color: white;
   }
 }
 </style>
